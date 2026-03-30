551beab0a9df7ef1d8373e04353fcfc908fdbbc4
A. M. Omi <amomi.self@gmail.com>
2026-03-30 12:00:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import OpenAI from "openai";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 
